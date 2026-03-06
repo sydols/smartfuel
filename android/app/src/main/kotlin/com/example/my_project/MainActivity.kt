651271d899cdcf566a04466b30a64402fca62457
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-03-06 01:43:18 +0000

    Connecting FlutterFlow  to Github and Adding Theme

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mycompany.smartfuel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- docs/BUSINESSPLAN.md
+++
@@ -1,1 +0,0 @@
-[text](https://docs.google.com/presentation/d/1l6qnf_NCxJYsL1JoF0uoAY4L35dtA3-pLRSfR-5S9AY/edit?usp=sharing)
